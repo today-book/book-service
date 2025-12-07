27f471eb01a0771e783a4d3bd96a62fe2e6ce0d8
hyeon48165 <hyeon48615@gmail.com>
2025-12-07 11:16:05 +0900

    [#11] Feat: 도서 조회 API 구현

--- src/main/java/org/todaybook/bookservice/application/service/BookService.java
+++ src/main/java/org/todaybook/bookservice/application/service/BookService.java
@@ -2,13 +2,12 @@ package org.todaybook.bookservice.application.service;
 
 import java.util.List;
 import java.util.UUID;
-import org.todaybook.bookservice.domain.BookId;
 import org.todaybook.bookservice.presentation.dto.BookRegisterRequest;
 import org.todaybook.bookservice.presentation.dto.BookResponse;
 import org.todaybook.bookservice.presentation.dto.BookUpdateRequest;
 
 public interface BookService {
-  List<BookResponse> getBooksByIds(List<BookId> ids);
+  List<BookResponse> getBooksByIds(List<UUID> ids);
 
   void create(BookRegisterRequest request);
 
