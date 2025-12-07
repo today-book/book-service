5a8dfe380e2654ff4a9f2fd3362ddbf54d7157f4
hyeon48165 <hyeon48615@gmail.com>
2025-12-07 11:17:50 +0900

    [#11] Style: Spotless 적용

--- src/main/java/org/todaybook/bookservice/presentation/dto/BookController.java
+++ src/main/java/org/todaybook/bookservice/presentation/dto/BookController.java
@@ -3,7 +3,6 @@ package org.todaybook.bookservice.presentation.dto;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
