206ca0af753a79253f62a7596290c244be287c2b
hyeon48165 <hyeon48615@gmail.com>
2025-12-24 04:25:50 +0900

    Feat: DLQ 전송 및 retry 설정 추가

--- src/main/java/org/todaybook/bookservice/infrastructure/kfaka/KafkaConfig.java
+++ src/main/java/org/todaybook/bookservice/infrastructure/kfaka/KafkaConfig.java
@@ -13,9 +13,12 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.util.backoff.FixedBackOff;
 import org.todaybook.bookservice.infrastructure.kfaka.dto.BookConsumeMessage;
 
 @Slf4j
@@ -23,6 +26,8 @@ import org.todaybook.bookservice.infrastructure.kfaka.dto.BookConsumeMessage;
 @Configuration
 @RequiredArgsConstructor
 public class KafkaConfig {
+
+  private final KafkaTemplate<String, Object> kafkaTemplate;
 
   private final KafkaProperties properties;
 
@@ -62,8 +67,17 @@ public class KafkaConfig {
     factory.setConsumerFactory(consumerFactory());
     factory.setBatchListener(true);
 
-    DefaultErrorHandler errorHandler = new DefaultErrorHandler();
-    errorHandler.addNotRetryableExceptions(SerializationException.class);
+    // DLQ 전송 담당
+    DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate);
+
+    // retry 3회 + backoff 1초
+    DefaultErrorHandler errorHandler =
+        new DefaultErrorHandler(recoverer, new FixedBackOff(1000L, 3));
+
+    // 재시도하면 의미 없는 예외들
+    errorHandler.addNotRetryableExceptions(
+        SerializationException.class, IllegalArgumentException.class);
+
     factory.setCommonErrorHandler(errorHandler);
 
     return factory;
